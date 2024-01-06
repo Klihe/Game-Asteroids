04fddc2554932946ea6bb041188bdb447d2235dc
Klihe <stepan.birke@gmail.com>
2024-01-06 13:17:50 +0100

    Clean - Player

--- modules/spirits/player.py
+++ modules/spirits/player.py
@@ -3,11 +3,9 @@ import pygame
 import math
 
 class Player:
-    def __init__(self, x, y, width, height, straight, left, right):
+    def __init__(self, x, y, key_straight, key_left, key_right) -> None:
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
         self.angle = 0
 
         self.rotation_speed_current = 0
@@ -15,16 +13,15 @@ class Player:
         self.movement_speed_current = 0
         self.movement_speed_max = 10
 
-        self.straight = straight
-        self.left = left
-        self.right = right
+        self.key_straight = key_straight
+        self.key_left = key_left
+        self.key_right = key_right
 
         self.image = pygame.image.load("source/rocket.png")
-        self.image_coordinates = self.image.get_rect(center=(self.x, self.y))
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rect = self.image.get_rect(center=(self.x, self.y))
 
-    def movement(self, keys):
-        if keys[self.straight] and self.movement_speed_current < self.movement_speed_max:
+    def movement(self, keys) -> None:
+        if keys[self.key_straight] and self.movement_speed_current < self.movement_speed_max:
             self.movement_speed_current += 0.5
         elif self.movement_speed_current > 0:
             self.movement_speed_current -= 0.25
@@ -32,26 +29,29 @@ class Player:
         self.x += int(self.movement_speed_current * math.sin(math.radians(self.angle)))
         self.y += int(self.movement_speed_current * math.cos(math.radians(self.angle)))
 
-        if keys[self.left] and self.rotation_speed_current < self.rotation_speed_max:
+        if keys[self.key_left] and self.rotation_speed_current < self.rotation_speed_max:
             self.rotation_speed_current += 3
-        elif keys[self.right] and self.rotation_speed_current > -self.rotation_speed_max:
+        elif keys[self.key_right] and self.rotation_speed_current > -self.rotation_speed_max:
             self.rotation_speed_current -= 3
         elif self.rotation_speed_current != 0:
             self.rotation_speed_current += 3 if self.rotation_speed_current < 0 else -3
 
         self.angle += self.rotation_speed_current / 10
 
-    def check_collision(self, other_spirit):
+    def fire(self, keys, func) -> None:
+        if keys[self.fire]:
+            func()
+
+    def check_collision(self, other_spirit) -> None:
         if self.rect.colliderect(other_spirit):
             print("Collision")
 
-    def update(self):
+    def update(self) -> None:
         self.image_rotate = pygame.transform.rotate(self.image, self.angle)
-        self.image_center = self.image_rotate.get_rect(center=self.image_coordinates.center)
-        self.image_coordinates = self.image.get_rect(center=(self.x, self.y))
+        self.image_center = self.image_rotate.get_rect(center=self.rect.center)
         
         self.rect.x = self.x
         self.rect.y = self.y
 
-    def draw(self, surface):
+    def draw(self, surface) -> None:
         surface.blit(self.image_rotate, self.image_center)
