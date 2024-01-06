62380bddbf0edc386612b113fcbe6c4744457a06
Klihe <stepan.birke@gmail.com>
2024-01-06 13:17:00 +0100

    Fix - Asteroid(Collision)

--- modules/spirits/asteroid.py
+++ modules/spirits/asteroid.py
@@ -9,18 +9,19 @@ class Asteroid:
         self.y = random.choice([random.randint(0, 500), random.randint(750, 1080)])
 
         self.movement_speed = random.randint(4, 5)
-        self.angle_start = random.randint(1, 359)
-        self.angle = random.randint(1, 359)
+        self.direction = random.randint(1, 359)
+        self.angle_rotate = random.randint(1, 359)
         
         self.image = pygame.image.load("source/asteroid.png")
         self.rect = self.image.get_rect(center=(self.x, self.y))
 
     def movement(self) -> None:
         if self.x > Config.WINDOW_WIDTH or self.x < 0 or self.y > Config.WINDOW_HEIGHT or self.y < 0:
-            self.angle_start += 45
+            self.direction += 45
+            self.movement_speed = -self.movement_speed
 
-        self.x += int(self.movement_speed * math.sin(math.radians(self.angle_start)))
-        self.y += int(self.movement_speed * math.cos(math.radians(self.angle_start)))
+        self.x += int(self.movement_speed * math.sin(math.radians(self.direction)))
+        self.y += int(self.movement_speed * math.cos(math.radians(self.direction)))
 
         self.rect.x = self.x
         self.rect.y = self.y
@@ -28,15 +29,17 @@ class Asteroid:
     def check_collision(self, other_spirit) -> None:
         cooldown = pygame.time.get_ticks()
         if self.rect.colliderect(other_spirit.rect) and cooldown > 250:
-            self.angle_start += 30
+            self.direction += 30
+            self.movement_speed = -self.movement_speed
             cooldown = 0
                 
             self.rect.x = self.x
             self.rect.y = self.y
 
     def update(self) -> None:
-        self.angle += 1
-        self.image_rotate = pygame.transform.rotate(self.image, self.angle)
+        self.angle_rotate += 1
+        self.image_rotate = pygame.transform.rotate(self.image, self.angle_rotate)
+        self.image_center = self.image_rotate.get_rect(center=self.rect.center)
 
     def draw(self, surface) -> None:
-        surface.blit(self.image_rotate, self.rect)
+        surface.blit(self.image_rotate, self.image_center)
