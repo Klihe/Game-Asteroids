027788da7f33b82fffee75f9bca11e4c1d93dcfa
Klihe <stepan.birke@gmail.com>
2024-01-17 14:10:44 +0100

    Init - Reworking

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# main.py
 import pygame
 from modules.config import Config
+from modules.game import Game
 
 pygame.init()
 
@@ -9,11 +11,19 @@ pygame.display.set_caption("Asteroids")
 clock = pygame.time.Clock()
 running = True
 
+game = Game()
+
 while running:
     clock.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    game.update(time=pygame.time.get_ticks(), keys=pygame.key.get_pressed())
+    game.draw(surface=window_main)
+
+    pygame.display.flip()
+
 
 pygame.quit()
---
+++ modules/game.py
@@ -0,0 +1,11 @@
+# game.py
+
+class Game:
+    def __init__(self) -> None:
+        pass
+    
+    def update(self, time, keys):
+        pass
+
+    def draw(self, surface):
+        pass
