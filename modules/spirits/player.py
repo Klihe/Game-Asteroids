a8d85164528e946406ef2c1830b475d6b6fcf5a4
Klihe <stepan.birke@gmail.com>
2024-01-18 15:31:28 +0100

    Add - Player(Comments)

--- modules/spirits/player.py
+++ modules/spirits/player.py
@@ -10,24 +10,26 @@ class Player:
         self.y = y
         self.angle = 0
 
+        # stats
         self.score = 0
         self.health = 100
         self.body_damage = 15
 
-        self.hit_detection_cooldown = 500
-        self.last_hit = 0
-
+        # speed
         self.rotation_speed = 1.5
         self.movement_speed = 2.5
         self.movement_speed_max = 10
 
+        # keys
         self.k_straight = k_straight
         self.k_left = k_left
         self.k_right = k_right
         self.k_fire = k_fire
 
-        self.image = pygame.image.load("source/playing/rocket.png")
-        self.image_rotate = pygame.transform.rotate(self.image, self.angle)
+        # image/body
+        self.src_image = pygame.image.load("source/playing/rocket.png")
+        self.image = pygame.transform.rotate(self.src_image, self.angle)
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image_rotate.get_rect(center=(self.x, self.y))
 
     def movement(self, keys) -> None:
@@ -44,14 +46,18 @@ class Player:
             self.angle -= self.rotation_speed + self.movement_speed // 5
 
     def update(self) -> None:
+        # update position
         self.x += int(self.movement_speed * math.sin(math.radians(self.angle)))
         self.y += int(self.movement_speed * math.cos(math.radians(self.angle)))
+
+        # boarders
         self.x = max(60, min(self.x, Config.WINDOW_WIDTH - 60))
         self.y = max(60, min(self.y, Config.WINDOW_HEIGHT - 60))
 
-        self.image_rotate = pygame.transform.rotate(self.image, self.angle)
-        self.rect = self.image_rotate.get_rect(center=(self.x, self.y))
-
+        # update image
+        self.image = pygame.transform.rotate(self.src_image, self.angle)
+        self.rect = self.image.get_rect(center=(self.x, self.y))
 
     def draw(self, surface) -> None:
-        surface.blit(self.image_rotate, self.rect)
+        # draw image
+        surface.blit(self.image, self.rect)
